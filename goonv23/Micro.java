a520d18d3bf60f2d8a01339e3fa0d2f60ca1eadd
bman12345678 <167883742+bman12345678@users.noreply.github.com>
2026-01-26 16:03:38 -0500

    Add files via upload

---
+++ goonv23/Micro.java
@@ -0,0 +1,9 @@
+package goonv23;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+@FunctionalInterface
+public interface Micro {
+    public void micro(Direction d, MapLocation dest) throws GameActionException;
+}
